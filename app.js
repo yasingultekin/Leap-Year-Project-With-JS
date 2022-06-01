4c2294f25615d270ea259f0b2b8832849cfb23dc
yasingultekin <yasingultekin13@gmail.com>
2022-06-01 12:13:56 +0300

    changes added.

--- app.js
+++ app.js
@@ -1,22 +1,18 @@
-let year = document.querySelector('input').value
+function entered(){
+    let year = document.querySelector("input").value
+    
+    if (year % 100 != 0 && year % 4 == 0){
+        alert(`Entered ${year} is a Leap year`)
+    } else if (year % 400 == 0){
+        alert(`Entered ${year} is a Leap year`)
+    } else{
+        alert(`Year ${year} is not a Leap Year`)
+    }
 
-function entered() {
-    if (year % 100 !=0 && year % 4 == 0) {
-    alert(`Your entered year ${year} is a Leap Year!`)
-} else if (year % 400 == 0) {
-    alert(`Your entered year ${year} is a Leap Year!`)
-} else {
-    alert(`Your entered year ${year} isn't a Leap Year!`)
 }
-}
 
-
-document.querySelector('input').addEventListener ('keydown', function(event) {
+document.querySelector("input").addEventListener ('keydown', function(event){
     if (event.key == 'Enter'){
         entered()
     }
-}) 
-
-
-
-
+})
